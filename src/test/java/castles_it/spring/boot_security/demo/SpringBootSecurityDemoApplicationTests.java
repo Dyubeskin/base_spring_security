0e49918b8048d220f6b8b706133c725d6385f936
Dyubeskin <nemeforza47@gmail.com>
2025-03-01 14:45:11 +0300

    base

---
+++ src/test/java/castles_it/spring/boot_security/demo/SpringBootSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package castles_it.spring.boot_security.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
